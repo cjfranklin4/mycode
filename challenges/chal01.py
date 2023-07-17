19d31f7d160b2d4329c04fd15288d2676411f7aa
Cayla Franklin <cjfranklin4@gmail.com>
2023-07-17 20:25:49 +0000

    Day 1 Challenge 1

---
+++ challenges/chal01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#ask user's name
+name = input("What is your name?")
+day_of_week = input("What day of the week is it?")
+
+#print user input
+print("Hello, " + name + "! Happy " + day_of_week + "!")
