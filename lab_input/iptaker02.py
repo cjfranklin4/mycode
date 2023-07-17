67f3d348c3c6e49818ac3cb4d782d92f85c12f54
Cayla Franklin <cjfranklin4@gmail.com>
2023-07-17 20:19:26 +0000

    learning about input function

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,9 @@
+#/usr/bin/env python3
+
+def main():
+    user_input = input("Please enter an IPv4 address:")
+    print("You told me the IPv4 address is: ", user_input)
+    vendor_name=input("What is the vendor name associated with the device? ")
+    print("The vendor associated with this device is ", vendor_name)
+
+main()
