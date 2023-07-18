9ff4c99aefb0c56d8536685f0833e9eb1bb94569
Cayla Franklin <cjfranklin4@gmail.com>
2023-07-18 17:11:17 +0000

    challenge 1 completed

---
+++ challenges/advanced_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+heroes = [["Marvel",["Avengers",["Iron Man","Captain America","Thor"]],["X-Men",["Wolverine","Cyclops","Storm"]]],["DC Comics",["Justice League",["Superman","Batman","Wonder Woman"]],["Teen Titans",["Robin","Starfire","Raven", "Cyborg", "Beast Boy"]]]]
+
+# Challenge 1
+print(f"My favorite Avenger is {heroes[0][1][1][1]}!")
+print(f"My favorite X-man is {heroes[0][2][1][0]}!")
+
+# Challenge 2
+
+#heroes[1][2][1]
