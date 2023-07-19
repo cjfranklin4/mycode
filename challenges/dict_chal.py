c0f785d31edac6053ab2ae6b4e06c30d20ac407f
Cayla Franklin <cjfranklin4@gmail.com>
2023-07-19 16:57:43 +0000

    dictionary challenge

---
+++ challenges/dict_chal.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+yuuki= {
+    "Yuuki Anzai": {
+        "Manga/Anime": "Devil’s Line",
+        "Species": ["Devil", "Human"],
+        "Birthday": "January 15th 1992",
+        "Occupation": {
+            "Formerly": "Police Officer for MPD Public Safety Division 5, F Squad",
+            "Currently": "Police Officer East Bay Public Security Section 1, Squad 3"
+        }
+    }
+}
+
+# add new key-value pair
+yuuki["Yuuki Anzai"].update({"Age":21})
+
+# print all keys
+yuu_obj = list(yuuki["Yuuki Anzai"].keys())
+print(yuu_obj)
+
+#choose a key
+choice = input("Choose a key from the list above: \n>")
+
+# return key and value
+choice_val = yuuki["Yuuki Anzai"].get(choice)
+name = list(yuuki.keys())
+print(f"{name[0]}'s {choice} is: {choice_val}")
