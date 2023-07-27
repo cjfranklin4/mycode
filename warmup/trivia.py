5ae1f9116a238b227975413642634c030ceacc0c
Cayla Franklin <cjfranklin4@gmail.com>
2023-07-27 15:33:11 +0000

    warmup updates

--- warmup/trivia.py
+++ warmup/trivia.py
@@ -10,27 +10,55 @@ def call_api():
     res = requests.get(url).json()
     return res
 
-def main():
+def get_quiz():
     results = call_api()
     #Part 1
-    #print(results)
+    question_list = []
+    correct_answers = []
+    answer_list = {}
+    key_name = 0
 
     # Part 2, 3, and some of 4
     for result in results['results']:
-        print(html.unescape(result['question']))
-        
+        ques = html.unescape(result['question'])
+        question_list.append(ques)
+
         #put answers in a list
         answers = []
-        answers.append(html.unescape(result['correct_answer']))
-        
+        correct = html.unescape(result['correct_answer'])
+        correct_answers.append(correct)
+        answers.append(correct)
+
         for answer in result['incorrect_answers']:
             answers.append(html.unescape(answer))
         # Randomize answers
         answers = random.sample(answers,len(answers))
-        
-        for answer in answers:
-            print(f"- {answer}")
-        print("\n")
+        answer_list.setdefault(key_name, answers)
+        key_name += 1
+
+    return question_list, answer_list, correct_answers
+
+def main():
+    questions, answers, correct = get_quiz()
+    score = 0
+    for index, question in enumerate(questions):
+        print(question)
+        for answer in answers[index]:
+            print(answer)
+        while True:
+            your_ans = input("Your Answer:")
+            your_ans.strip()
+            if your_ans in answers[index]:
+                print("You got it right!")
+                score+= 1
+                break
+            else:
+                print(f"Sorry, wrong answer. The correct answer was {correct[index]}.")
+                break
+
+            print("\n")
+    print(f"You got {score}/10 correct!")
+    print("Thanks for playing!")
 
 if __name__ == "__main__":
     main()
