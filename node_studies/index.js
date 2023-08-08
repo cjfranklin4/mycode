7546e673951cfc502aef57b89e2f9461b0f84b3f
Cayla Franklin <cjfranklin4@gmail.com>
2023-08-08 19:32:11 +0000

    js practice

---
+++ node_studies/index.js
@@ -0,0 +1,9 @@
+const axios = require('axios'); // this semicolon is optional 
+axios.get('http://api.open-notify.org/astros.json')
+    .then(response => {
+    console.log(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+    
