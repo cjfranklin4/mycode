840f9801d055bc340a45f8ba25f2833209027e9b
Cayla Franklin <cjfranklin4@gmail.com>
2023-08-10 21:33:40 +0000

    updated db project queries

--- db-proj/query_table.py
+++ db-proj/query_table.py
@@ -34,4 +34,38 @@ print(f"The list of chores for {choice} is:")
 for chore in chores_day:
     print(chore[0])
 
+#Chores list from db
+c.execute('''
+    SELECT chores.type
+    FROM chores''')
+res_c = c.fetchall()
+chores_list = []
+for chore in res_c:
+    chores_list.append(chore[0])
+#print(chores_list)
+
+#Prompt for chores
+while True:
+    print("\nChoose a chore to see what day it is planned for: ")
+    for chore in chores_list:
+        print(chore)
+    chore_choice = input(">").strip()
+    if chore_choice in chores_list:
+        break
+
+#Find all days where that chore is planned for this week
+c.execute('''
+    SELECT weekdays.weekday
+    FROM weekdays
+    JOIN weekday_chores on weekdays.id = weekday_chores.weekday_id
+    JOIN chores on chores.id = weekday_chores.chores_id
+    WHERE chores.type = ?
+''', (chore_choice,))
+week_days = c.fetchall()
+
+#print(week_days)
+print(f"The days of the week for {chore_choice} is:")
+for day in week_days:
+    print(day[0])
+
 conn.close()
